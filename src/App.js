f15bb2caa29d2806b698ff0fae1115aacc1206f5
jmpark6846 <jmpark6846@gmail.com>
2018-04-26 11:14:43 +0900

    SocialCard, UrlInput 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/*
+App - 
+  UrlInput
+  SocialCard
+
+url 입력 - UrlInput
+fetch - App
+socialcard 보여주기 - SocialCard
+
+*/
+const UrlInput = () => 
+  <div className="url-input">
+    <input type="text" placeholder="URL을 입력하세요."/>
+  </div>
+
+const SocialCard = () => 
+  <div className="social-card">
+    SocialCard
+  </div>
+
 class App extends Component {
-  render() {
+  render() { 
     return (
-      
+      <div className="App">
+        <UrlInput></UrlInput>
+        <SocialCard></SocialCard>
+      </div> 
     );
   }
 }
 
 export default App;
+
+// http://opengraph.io/api/1.1/site/http%3A%2F%2Fdavidwalsh.name?app_id=5ae0777b9b03547407c63e6d
